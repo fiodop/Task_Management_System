efd10eb88d584ef3f5c996037d87d3d53f4ac241
fiodop <artemklucarov666@gmail.com>
2025-01-19 16:26:13 +0300

    first commit

---
+++ src/main/java/com/taskmanagementsystem/controller/AdminController.java
@@ -0,0 +1,4 @@
+package com.taskmanagementsystem.controller;
+@Re
+public class AdminController {
+}
---
+++ src/main/java/com/taskmanagementsystem/service/AuthService.java
@@ -0,0 +1,53 @@
+package com.taskmanagementsystem.service;
+
+import com.taskmanagementsystem.model.dto.AuthenticationRequest;
+import com.taskmanagementsystem.model.dto.AuthenticationResponse;
+import com.taskmanagementsystem.model.dto.RegisterRequest;
+import com.taskmanagementsystem.model.entity.AppUser;
+import com.taskmanagementsystem.model.enums.Role;
+import com.taskmanagementsystem.repository.AppUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final AppUserRepository appUserRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = AppUser.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        appUserRepository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        var user = appUserRepository.findByUsername(request.getUsername()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
